18a39d582fd3a03643274603ec66a65cd63a65d5
Xuejie Xiao <xxuejie@gmail.com>
2025-04-09 01:56:34 +0000

    Provide a dummy fuzzing entrypoint function in binary to text converter

--- amalgamated/fuzzing_syscalls_all_in_one.h
+++ amalgamated/fuzzing_syscalls_all_in_one.h
@@ -5934,9 +5934,22 @@ int main() {
  */
 /* fuzzing_syscalls_internal.h has already been included. */
 #include <google/protobuf/text_format.h>
+#include <assert.h>
 #include <fstream>
 #include <iostream>
 using namespace std;
+
+/*
+ * Converter is a standalone utility, meaning this shall be a dummy
+ * function
+ */
+int CKB_FUZZING_ENTRYPOINT(int argc, char* argv[]) {
+  (void)argc;
+  (void)argv;
+
+  assert(false);
+  return -1;
+}
 
 #undef main
 int main(int argc, char* argv[]) {
@@ -5956,7 +5969,7 @@ int main(int argc, char* argv[]) {
   }
 
   string output;
-  google::protobuf::TextFormat::PrintToString(syscalls, &output);
+  assert(google::protobuf::TextFormat::PrintToString(syscalls, &output));
   cout << output;
   return 0;
 }
