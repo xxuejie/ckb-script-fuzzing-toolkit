d6a7c3b1b517b0e7b81ee2ba1167e77984947357
Xuejie Xiao <xxuejie@gmail.com>
2025-05-30 02:47:30 +0000

    Bug fix

--- src/syscalls/argv_builder.h
+++ src/syscalls/argv_builder.h
@@ -19,11 +19,14 @@ class ArgvBuilder {
 
   void push(const char* arg) {
     // Each argv is aligned by 8 bytes.
-    size_t current_len = strlen(arg);
+    size_t current_len = strlen(arg) + 1;
     size_t rounded_len = ((current_len + 7) / 8) * 8;
 
     buffer_ = (char*)realloc(buffer_, length_ + rounded_len);
-    memcpy(&buffer_[length_], arg, rounded_len);
+    memcpy(&buffer_[length_], arg, current_len);
+    if (rounded_len > current_len) {
+      memset(&buffer_[length_ + current_len], 0, rounded_len - current_len);
+    }
 
     offsets_.push_back(length_);
     length_ += rounded_len;
@@ -35,9 +38,12 @@ class ArgvBuilder {
     // At the start, each argv item requires a pointer, plus a NULL pointer
     size_t pointers_size = (offsets_.size() + 1) * sizeof(size_t);
     char* flattened_argv = (char*)malloc(pointers_size + length_);
-    memcpy(&flattened_argv[pointers_size], buffer_, length_);
+    if (length_ > 0) {
+      memcpy(&flattened_argv[pointers_size], buffer_, length_);
+    }
     for (size_t i = 0; i < offsets_.size(); i++) {
-      ((size_t*)flattened_argv)[i] = (size_t)(&flattened_argv[offsets_[i]]);
+      ((size_t*)flattened_argv)[i] =
+          (size_t)(&flattened_argv[pointers_size + offsets_[i]]);
     }
     ((size_t*)flattened_argv)[offsets_.size()] = 0;
     return (char**)flattened_argv;
