da3e93aca4207029723f499bd227fabd045b7a7e
Xuejie Xiao <xxuejie@gmail.com>
2025-06-18 06:46:50 +0000

    Mark fdp crate build with C++ support

--- crates/fdp-ckb-syscalls/build.rs
+++ crates/fdp-ckb-syscalls/build.rs
@@ -2,6 +2,7 @@ fn main() {
     let mut build = cc::Build::new();
     build
         .file("c_src/syscalls/fuzzed_data_provider.cc")
+        .cpp(true)
         .include("c_include")
         .include("c_src")
         .include("c_deps/ckb-c-stdlib")
